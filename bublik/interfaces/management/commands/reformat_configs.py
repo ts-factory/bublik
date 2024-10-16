38b843519a76a9e8384b02270134cd6422f35e8b
Natalia Rybchenko <natalia.rybchenko@oktetlabs.ru>
2024-10-16 19:30:17 +0300

    management: add ability to reformat configs
    Add a command to update the configurations of the passed type and name.
    
    Signed-off-by: Natalia Rybchenko <natalia.rybchenko@oktetlabs.ru>

---
+++ bublik/interfaces/management/commands/reformat_configs.py
@@ -0,0 +1,81 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2024 OKTET Labs Ltd. All rights reserved.
+
+from django.core.management.base import BaseCommand
+from django.db.models import Q
+from rest_framework import serializers
+
+from bublik.data.models import Config, ConfigTypes
+from bublik.data.serializers import ConfigSerializer
+
+
+class Command(BaseCommand):
+    def get_configs(self, options):
+        '''
+        Get a list of configurations filtered by the passed types and names.
+        '''
+        configs = Config.objects.all()
+        if not options['type'] and not options['name']:
+            return configs
+
+        if not options['name']:
+            # we only have valid types as a result of parsing arguments
+            return configs.filter(type__in=options['type'])
+
+        if not options['type']:
+            # name validation is possible only if there is a type
+            return configs.filter(name__in=options['name'])
+
+        types_names = {t: options['name'] for t in options['type']}
+        for config_type, config_names in types_names.items():
+            valid_config_names = []
+            for config_name in config_names:
+                serializer = ConfigSerializer(data={'type': config_type, 'name': config_name})
+                try:
+                    serializer.validate_type(config_type)
+                    serializer.validate_name(config_name)
+                    valid_config_names.append(config_name)
+                except serializers.ValidationError as ve:
+                    self.stdout.write(
+                        self.style.WARNING(
+                            f'Invalid configuration type-name "{config_type}-{config_name}": '
+                            f'{type(ve).__name__}: {ve}. Skipped.',
+                        ),
+                    )
+            types_names[config_type] = valid_config_names
+
+        if types_names:
+            types_names_filter = Q()
+            for config_type, config_names in types_names.items():
+                types_names_filter |= Q(type=config_type, name__in=config_names)
+            return configs.filter(types_names_filter)
+
+        return None
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '-t',
+            '--type',
+            type=str,
+            choices=ConfigTypes.all(),
+            nargs='+',
+            help='Types of configurations for reformatting',
+        )
+        parser.add_argument(
+            '-n',
+            '--name',
+            type=str,
+            nargs='+',
+            help='Names of configurations for reformatting',
+        )
+
+    def handle(self, *args, **options):
+        configs = self.get_configs(options)
+        if not configs:
+            self.stdout.write(
+                self.style.WARNING(
+                    'No valid configurations were found. Reformatting is skipped.',
+                ),
+            )
+
+        # TODO: add reformatting functions
