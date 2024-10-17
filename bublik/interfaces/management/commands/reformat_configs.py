7d654ea466afb42423047ad2f88291138c4d478f
Natalia Rybchenko <natalia.rybchenko@oktetlabs.ru>
2024-10-17 14:45:23 +0300

    management: update reformat configs command
    Add a reformatting of the report configuration according to the new
    x-axis and sequences settings structure.
    
    Signed-off-by: Natalia Rybchenko <natalia.rybchenko@oktetlabs.ru>

--- bublik/interfaces/management/commands/reformat_configs.py
+++ bublik/interfaces/management/commands/reformat_configs.py
@@ -10,6 +10,121 @@ from bublik.data.serializers import ConfigSerializer
 
 
 class Command(BaseCommand):
+    def update_axis_x_structure(self, configs):
+        '''
+        Reformat passed report configs content:
+        "axis_x": <str> -> "axis_x": {"arg": <str>}
+        '''
+        report_configs = configs.filter(type=ConfigTypes.REPORT)
+        for report_config in report_configs:
+            try:
+                config_data = report_config.content
+                modified = False
+                for test_name, test_config_data in config_data['tests'].items():
+                    if isinstance(test_config_data['axis_x'], str):
+                        modified = True
+                        test_config_data['axis_x'] = {
+                            'arg': test_config_data['axis_x'],
+                        }
+                        config_data['tests'][test_name] = test_config_data
+
+                if modified:
+                    serializer = ConfigSerializer(
+                        instance=report_config,
+                        data={'content': config_data},
+                        partial=True,
+                    )
+                    serializer.is_valid(raise_exception=True)
+                    report_config.content = serializer.validated_data['content']
+                    report_config.save()
+
+                    self.stdout.write(
+                        self.style.SUCCESS(
+                            f'{report_config.name} v{report_config.version}: '
+                            'the x-axis settings structure has been successfully updated!',
+                        ),
+                    )
+                else:
+                    self.stdout.write(
+                        self.style.SUCCESS(
+                            f'{report_config.name} v{report_config.version}: '
+                            'the x-axis settings structure already updated!',
+                        ),
+                    )
+            except Exception as e:
+                self.stdout.write(
+                    self.style.ERROR(
+                        f'{report_config.name} v{report_config.version}: '
+                        f'failed to update x-axis settings structure: {type(e).__name__}: {e}',
+                    ),
+                )
+
+    def update_seq_settings_structure(self, configs):
+        '''
+        Reformat passed report configs content:
+        Add the 'sequences' foreign key for all sequences settings
+        (sequence_group_arg, percentage_base_value, sequence_name_conversion).
+        Rename 'sequence_name_conversion' to 'arg_vals_labels'.
+        '''
+        report_configs = configs.filter(type=ConfigTypes.REPORT)
+        for report_config in report_configs:
+            try:
+                config_data = report_config.content
+                modified = False
+                for test_name, test_config_data in config_data['tests'].items():
+                    if 'sequences' not in test_config_data:
+                        modified = True
+                        if test_config_data['sequence_group_arg'] is not None:
+                            test_config_data['sequences'] = {
+                                'arg': test_config_data['sequence_group_arg'],
+                            }
+                            if test_config_data['percentage_base_value'] is not None:
+                                test_config_data['sequences'][
+                                    'percentage_base_value'
+                                ] = test_config_data['percentage_base_value']
+                            if test_config_data['sequence_name_conversion']:
+                                test_config_data['sequences'][
+                                    'arg_vals_labels'
+                                ] = test_config_data['sequence_name_conversion']
+
+                        test_config_data.pop('sequence_group_arg')
+                        test_config_data.pop('percentage_base_value')
+                        test_config_data.pop('sequence_name_conversion')
+
+                        config_data['tests'][test_name] = test_config_data
+
+                if modified:
+                    serializer = ConfigSerializer(
+                        instance=report_config,
+                        data={'content': config_data},
+                        partial=True,
+                    )
+                    serializer.is_valid(raise_exception=True)
+                    report_config.content = serializer.validated_data['content']
+                    report_config.save()
+
+                    self.stdout.write(
+                        self.style.SUCCESS(
+                            f'{report_config.name} v{report_config.version}: '
+                            'the sequences settings structure has been successfully updated!',
+                        ),
+                    )
+                else:
+                    self.stdout.write(
+                        self.style.SUCCESS(
+                            f'{report_config.name} v{report_config.version}: '
+                            'the sequences settings structure already updated!',
+                        ),
+                    )
+            except Exception as e:
+                self.stdout.write(
+                    self.style.ERROR(
+                        f'{report_config.name} v{report_config.version}: '
+                        'failed to update sequences settings structure: '
+                        f'{type(e).__name__}: {e}',
+                    ),
+                )
+
     def get_configs(self, options):
         '''
         Get a list of configurations filtered by the passed types and names.
@@ -78,4 +193,5 @@ class Command(BaseCommand):
                 ),
             )
 
-        # TODO: add reformatting functions
+        self.update_axis_x_structure(configs)
+        self.update_seq_settings_structure(configs)
