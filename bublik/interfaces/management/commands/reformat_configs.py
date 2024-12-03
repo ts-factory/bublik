890d95ed2bfa4f5ce6ea22ae939fc9bd6db21cb1
Natalia Rybchenko <natalia.rybchenko@oktetlabs.ru>
2024-12-03 05:29:21 +0300

    management: make config reformating more scalable
    Implement a step-based pipeline dispatcher for reformatting
    configurations, making the process more flexible, secure, controlled,
    and easily scalable.
    
    Signed-off-by: Natalia Rybchenko <natalia.rybchenko@oktetlabs.ru>

--- bublik/interfaces/management/commands/reformat_configs.py
+++ bublik/interfaces/management/commands/reformat_configs.py
@@ -5,19 +5,12 @@ from django.core.management.base import BaseCommand
 from django.db.models import Q
 from rest_framework import serializers
 
-from bublik.data.models import Config, ConfigTypes, GlobalConfigNames
+from bublik.core.config.reformatting.dispatcher import (
+    ConfigReformatDispatcher,
+    ConfigReformatStatuses,
+)
+from bublik.data.models import Config, ConfigTypes
 from bublik.data.serializers import ConfigSerializer
-
-
-def update_config_content(config_instance, new_content):
-    serializer = ConfigSerializer(
-        instance=config_instance,
-        data={'content': new_content},
-        partial=True,
-    )
-    serializer.is_valid(raise_exception=True)
-    config_instance.content = serializer.validated_data['content']
-    config_instance.save()
 
 
 class Command(BaseCommand):
@@ -63,194 +56,6 @@ class Command(BaseCommand):
 
         return None
 
-    def update_axis_x_structure(self, configs):
-        '''
-        Reformat passed report configs content:
-        "axis_x": <str> -> "axis_x": {"arg": <str>}
-        '''
-        report_configs = configs.filter(type=ConfigTypes.REPORT)
-        for report_config in report_configs:
-            try:
-                config_data = report_config.content
-                modified = False
-                for test_name, test_config_data in config_data['tests'].items():
-                    if isinstance(test_config_data['axis_x'], str):
-                        modified = True
-                        test_config_data['axis_x'] = {
-                            'arg': test_config_data['axis_x'],
-                        }
-                        config_data['tests'][test_name] = test_config_data
-
-                if modified:
-                    update_config_content(report_config, config_data)
-                    self.stdout.write(
-                        self.style.SUCCESS(
-                            f'{report_config.name} v{report_config.version}: '
-                            'the x-axis settings structure has been successfully updated!',
-                        ),
-                    )
-                else:
-                    self.stdout.write(
-                        self.style.SUCCESS(
-                            f'{report_config.name} v{report_config.version}: '
-                            'the x-axis settings structure already updated!',
-                        ),
-                    )
-            except Exception as e:
-                self.stdout.write(
-                    self.style.ERROR(
-                        f'{report_config.name} v{report_config.version}: '
-                        f'failed to update x-axis settings structure: {type(e).__name__}: {e}',
-                    ),
-                )
-
-    def update_seq_settings_structure(self, configs):
-        '''
-        Reformat passed report configs content:
-        Add the 'sequences' foreign key for all sequences settings
-        (sequence_group_arg, percentage_base_value, sequence_name_conversion).
-        Rename 'sequence_name_conversion' to 'arg_vals_labels'.
-        '''
-        report_configs = configs.filter(type=ConfigTypes.REPORT)
-        for report_config in report_configs:
-            try:
-                config_data = report_config.content
-                modified = False
-                for test_name, test_config_data in config_data['tests'].items():
-                    if 'sequences' not in test_config_data:
-                        modified = True
-                        if test_config_data['sequence_group_arg'] is not None:
-                            test_config_data['sequences'] = {
-                                'arg': test_config_data['sequence_group_arg'],
-                            }
-                            if test_config_data['percentage_base_value'] is not None:
-                                test_config_data['sequences']['percentage_base_value'] = (
-                                    test_config_data['percentage_base_value']
-                                )
-                            if test_config_data['sequence_name_conversion']:
-                                test_config_data['sequences']['arg_vals_labels'] = (
-                                    test_config_data['sequence_name_conversion']
-                                )
-
-                        test_config_data.pop('sequence_group_arg')
-                        test_config_data.pop('percentage_base_value')
-                        test_config_data.pop('sequence_name_conversion')
-
-                        config_data['tests'][test_name] = test_config_data
-
-                if modified:
-                    update_config_content(report_config, config_data)
-                    self.stdout.write(
-                        self.style.SUCCESS(
-                            f'{report_config.name} v{report_config.version}: '
-                            'the sequences settings structure has been successfully updated!',
-                        ),
-                    )
-                else:
-                    self.stdout.write(
-                        self.style.SUCCESS(
-                            f'{report_config.name} v{report_config.version}: '
-                            'the sequences settings structure already updated!',
-                        ),
-                    )
-            except Exception as e:
-                self.stdout.write(
-                    self.style.ERROR(
-                        f'{report_config.name} v{report_config.version}: '
-                        'failed to update sequences settings structure: '
-                        f'{type(e).__name__}: {e}',
-                    ),
-                )
-
-    def update_dashboard_header_structure(self, configs):
-        '''
-        Reformat passed global per_conf configs content:
-        DASHBOARD_HEADER: {<key>: <label>} ->
-        DASHBOARD_HEADER: [{"key": <key>, "label": <label>}]
-        '''
-        per_conf_configs = configs.filter(
-            type=ConfigTypes.GLOBAL,
-            name=GlobalConfigNames.PER_CONF,
-        )
-        for per_conf_config in per_conf_configs:
-            try:
-                config_data = per_conf_config.content
-                if isinstance(config_data['DASHBOARD_HEADER'], dict):
-                    config_data['DASHBOARD_HEADER'] = [
-                        {'key': key, 'label': label}
-                        for key, label in config_data['DASHBOARD_HEADER'].items()
-                    ]
-                    update_config_content(per_conf_config, config_data)
-                    self.stdout.write(
-                        self.style.SUCCESS(
-                            f'{per_conf_config.name} v{per_conf_config.version}: '
-                            'the dashboard header structure has been successfully updated!',
-                        ),
-                    )
-                else:
-                    self.stdout.write(
-                        self.style.SUCCESS(
-                            f'{per_conf_config.name} v{per_conf_config.version}: '
-                            'the dashboard header structure already updated!',
-                        ),
-                    )
-            except Exception as e:
-                self.stdout.write(
-                    self.style.ERROR(
-                        f'{per_conf_config.name} v{per_conf_config.version}: '
-                        'failed to update the dashboard header structure: '
-                        f'{type(e).__name__}: {e}',
-                    ),
-                )
-
-    def update_csrf_trusted_origins(self, configs):
-        '''
-        Reformat passed global per_conf configs content:
-        CSRF_TRUSTED_ORIGINS: ["http://origin1", "origin2", "https://origin3"] ->
-        CSRF_TRUSTED_ORIGINS: ["http://origin1", "https://origin2", "https://origin3"]
-        '''
-        per_conf_configs = configs.filter(
-            type=ConfigTypes.GLOBAL,
-            name=GlobalConfigNames.PER_CONF,
-        )
-        for per_conf_config in per_conf_configs:
-            try:
-                config_data = per_conf_config.content
-                csrf_trusted_origins = config_data.get('CSRF_TRUSTED_ORIGINS', [])
-                updated_csrf_trusted_origins = [
-                    (
-                        origin
-                        if origin.startswith(('http://', 'https://'))
-                        else f'https://{origin}'
-                    )
-                    for origin in csrf_trusted_origins
-                ]
-                if csrf_trusted_origins != updated_csrf_trusted_origins:
-                    config_data['CSRF_TRUSTED_ORIGINS'] = updated_csrf_trusted_origins
-                    update_config_content(per_conf_config, config_data)
-                    self.stdout.write(
-                        self.style.WARNING(
-                            f'{per_conf_config.name} v{per_conf_config.version}: '
-                            'HTTPS has been added to CSRF trusted origins without a scheme. '
-                            'Update manually if HTTP is needed.',
-                        ),
-                    )
-                else:
-                    self.stdout.write(
-                        self.style.SUCCESS(
-                            f'{per_conf_config.name} v{per_conf_config.version}: '
-                            'CSRF trusted origins already updated!',
-                        ),
-                    )
-            except Exception as e:
-                self.stdout.write(
-                    self.style.ERROR(
-                        f'{per_conf_config.name} v{per_conf_config.version}: '
-                        'failed to update CSRF trusted origins: '
-                        f'{type(e).__name__}: {e}',
-                    ),
-                )
-
     def add_arguments(self, parser):
         parser.add_argument(
             '-t',
@@ -269,6 +74,7 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
+        # get configs
         configs = self.get_configs(options)
         if not configs:
             self.stdout.write(
@@ -276,8 +82,35 @@ class Command(BaseCommand):
                     'No valid configurations were found. Reformatting is skipped.',
                 ),
             )
+        else:
+            # reformat configs
+            dispatcher = ConfigReformatDispatcher()
+            reformatting_summary = {}
+            for config in configs:
+                config_label = f'{config.name} ({config.type}, v{config.version})'
+                self.stdout.write(f'{config_label} reformatting:')
+                reformatting_status = dispatcher.reformat_config(config)
+                reformatting_summary[config_label] = reformatting_status
 
-        self.update_axis_x_structure(configs)
-        self.update_seq_settings_structure(configs)
-        self.update_dashboard_header_structure(configs)
-        self.update_csrf_trusted_origins(configs)
+            # show the reformatting summary
+            self.stdout.write('\n=========================================================')
+            self.stdout.write('Configurations reformatting summary status:')
+            for config_label, reformatting_status in reformatting_summary.items():
+                if reformatting_status == ConfigReformatStatuses.SUCCESS:
+                    self.stdout.write(
+                        self.style.SUCCESS(
+                            f'\t{config_label}: successfully reformatted!',
+                        ),
+                    )
+                elif reformatting_status == ConfigReformatStatuses.SKIPPED:
+                    self.stdout.write(f'\t{config_label}: reformatting is skipped!')
+                elif reformatting_status == ConfigReformatStatuses.FAILED:
+                    self.stdout.write(
+                        self.style.ERROR(
+                            f'\t{config_label}: failed to reformat!',
+                        ),
+                    )
+                else:
+                    msg = 'Unknown reformatting status'
+                    raise ValueError(msg)
+            self.stdout.write('=========================================================')
