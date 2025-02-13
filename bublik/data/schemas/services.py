56dfec2863ba3c7a0ff709f25f7383e328b962c1
Natalia Rybchenko <natalia.rybchenko@oktetlabs.ru>
2025-02-13 11:16:52 +0300

    deploy: update deployment to initialize configs in the DB
    Update the deployment process to initialize configurations in the
    database using default values from JSON schemas, instead of creating
    files from templates, to align with the new configuration storage
    method (database instead of directory).
    
    Signed-off-by: Natalia Rybchenko <natalia.rybchenko@oktetlabs.ru>

--- bublik/data/schemas/services.py
+++ bublik/data/schemas/services.py
@@ -11,3 +11,23 @@ SCHEMAS_DIR = os.path.dirname(__file__)
 def load_schema(schema_name):
     with open(os.path.join(SCHEMAS_DIR, f'{schema_name}.json')) as schema_file:
         return json.load(schema_file)
+
+
+def generate_content(schema):
+    if 'default' in schema:
+        return schema['default']
+
+    schema_type = schema.get('type')
+
+    if schema_type == 'object':
+        obj = {}
+        for key, prop in schema.get('properties', {}).items():
+            value = generate_content(prop)
+            if value not in [None, {}]:
+                obj[key] = value
+        return obj
+
+    if schema_type == 'array':
+        return [generate_content(schema['items'])] if 'default' in schema.get('items', {}) else []
+
+    return None
---
+++ bublik/interfaces/management/commands/initialize_configs.py
@@ -0,0 +1,46 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2025 OKTET Labs Ltd. All rights reserved.
+
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+
+from bublik.core.config.services import ConfigServices
+from bublik.data.models import (
+    Config,
+    ConfigTypes,
+    GlobalConfigNames,
+)
+from bublik.data.schemas.services import generate_content
+from bublik.data.serializers import ConfigSerializer
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        '''
+        Initialize global configurations according to JSON schemas.
+        '''
+        call_command(
+            'migrate_configs',
+        )
+
+        self.stdout.write('\nInitialize configurations if they are not exist:')
+        for config_name in GlobalConfigNames.all():
+            try:
+                Config.objects.get_global(config_name)
+                self.stdout.write(f'{config_name}: already exist!')
+            except ObjectDoesNotExist:
+                json_schema = ConfigServices.get_schema(
+                    ConfigTypes.GLOBAL,
+                    config_name,
+                )
+                ConfigSerializer.initialize(
+                    {
+                        'type': ConfigTypes.GLOBAL,
+                        'name': config_name,
+                        'content': generate_content(json_schema),
+                    },
+                )
+                self.stdout.write(
+                    self.style.SUCCESS(f'{config_name}: succesfully initialized!'),
+                )
