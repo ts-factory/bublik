b62a43c7dde3813f175f5729040a0b31dbd2fda0
Danil Kostromin <danil.kostromin@icloud.com>
2025-03-05 01:48:45 +0300

    docker: add json logs proxy endpoint
    We need this because if we have locally deployed bublik
    but want to view logs in the UI from remote source we need
    to serve `node.json` files through proxy to avoid CORS issues.
    
    Signed-off-by: Danil Kostromin <danil.kostromin@icloud.com>

--- bublik/interfaces/api_v2/log.py
+++ bublik/interfaces/api_v2/log.py
@@ -3,6 +3,10 @@
 
 import os.path
 
+import requests
+from urllib.parse import urlparse
+from django.http import HttpResponse
+from django.conf import settings
 from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.mixins import RetrieveModelMixin
@@ -63,6 +67,18 @@ class LogViewSet(RetrieveModelMixin, GenericViewSet):
 
         url = os.path.join(run_source_link, json_tail + '.json')
         artifacts_url = os.path.join(run_source_link, json_tail, 'artifacts.json')
+
+        if settings.ENABLE_JSON_LOGS_PROXY:
+            parsed_url = urlparse(request.build_absolute_uri())
+            origin = f"{'https' if settings.SECURE_HTTP else 'http'}://{parsed_url.netloc}"
+            forwarding_url = f'{origin}{settings.PREFIX}/api/v2/logs/proxy/?url={url}'
+            artifacts_url = f'{origin}{settings.PREFIX}/api/v2/logs/proxy/?url={artifacts_url}'
+
+            return Response(
+                data={'url': forwarding_url, 'artifacts_url': artifacts_url},
+                status=status.HTTP_200_OK,
+            )
+
         return Response(
             data={'url': url, 'artifacts_url': artifacts_url},
             status=status.HTTP_200_OK,
@@ -77,3 +93,31 @@ class LogViewSet(RetrieveModelMixin, GenericViewSet):
 
         result = self.get_object()
         return Response(data={'url': get_result_log(result)}, status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=['get'])
+    def proxy(self, request):
+        r'''
+        Forward the request to the given URL and return the response data.
+        Route: /api/v2/logs/proxy/?url=<forwarding_url>
+        '''
+        forward_url = request.query_params.get('url')
+        if not forward_url:
+            return Response(
+                status=status.HTTP_400_BAD_REQUEST,
+                data={'message': 'URL parameter is missing.'},
+            )
+
+        try:
+            response = requests.get(forward_url)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            return Response(
+                status=status.HTTP_502_BAD_GATEWAY,
+                data={'message': f'Error forwarding request: {str(e)}'},
+            )
+
+        return HttpResponse(
+            content=response.content,
+            status=response.status_code,
+            content_type=response.headers.get('Content-Type', 'application/octet-stream'),
+        )
