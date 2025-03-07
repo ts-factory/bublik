59f2b1db0b5b1f275dae76a8b54fa6902933f428
Natalia Rybchenko <natalia.rybchenko@oktetlabs.ru>
2025-03-07 23:08:31 +0300

    management: update config reformatting labels with project
    Issue: #182
    Signed-off-by: Natalia Rybchenko <natalia.rybchenko@oktetlabs.ru>

--- bublik/interfaces/management/commands/reformat_configs.py
+++ bublik/interfaces/management/commands/reformat_configs.py
@@ -99,7 +99,11 @@ class Command(BaseCommand):
             dispatcher = ConfigReformatDispatcher()
             reformatting_summary = {}
             for config in configs:
-                config_label = f'{config.name} ({config.type}, v{config.version})'
+                config_label = (
+                    f'{config.name} ({config.type}, '
+                    f'{config.project.name if config.project else "default"}, '
+                    f'v{config.version})'
+                )
                 self.stdout.write(f'{config_label} reformatting:')
                 reformatting_status = dispatcher.reformat_config(config)
                 reformatting_summary[config_label] = reformatting_status
