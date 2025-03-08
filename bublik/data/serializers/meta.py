a4f046b6a908a776c1d395c7c7e23a5f0f4181ce
Natalia Rybchenko <natalia.rybchenko@oktetlabs.ru>
2025-03-08 20:14:02 +0300

    project: add data validation and preprocessing
    Extend of MetaSerializer for validation and preprocessing of data,
    as well as creating Meta objects corresponding to projects.
    
    Signed-off-by: Natalia Rybchenko <natalia.rybchenko@oktetlabs.ru>

--- bublik/data/serializers/meta.py
+++ bublik/data/serializers/meta.py
@@ -4,7 +4,11 @@
 
 from typing import ClassVar
 
+from rest_framework import serializers
+
 from bublik.core.hash_system import HashedModelSerializer
+from bublik.core.meta.categorization import categorize_meta
+from bublik.core.shortcuts import serialize
 from bublik.core.utils import empty_to_none
 from bublik.data.models import Meta
 
@@ -27,3 +31,29 @@ class MetaSerializer(HashedModelSerializer):
     def to_internal_value(self, data):
         empty_to_none(data, ['name', 'comment'])
         return super().to_internal_value(data)
+
+
+class ProjectMetaSerializer(MetaSerializer):
+    project_name = serializers.CharField(
+        source='value',
+        help_text=(
+            'This is the name field representing the value of the meta object, '
+            'corresponding to the project'
+        ),
+    )
+
+    class Meta:
+        model = Meta
+        fields: ClassVar[list] = ['id', 'project_name']
+
+    def get_or_create(self):
+        m_data = {
+            'name': 'PROJECT',
+            'type': 'label',
+            'value': self.initial_data['project_name'],
+        }
+        meta_serializer = serialize(self.__class__.__bases__[0], m_data)
+        project_meta, created = meta_serializer.get_or_create()
+        if created:
+            categorize_meta(project_meta)
+        return project_meta, created
