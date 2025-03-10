82922a1365205da22ab52110fdaa17c10c7c7393
Natalia Rybchenko <natalia.rybchenko@oktetlabs.ru>
2025-03-10 04:52:06 +0300

    server: update the project source
    Signed-off-by: Natalia Rybchenko <natalia.rybchenko@oktetlabs.ru>

--- bublik/interfaces/api_v2/server.py
+++ bublik/interfaces/api_v2/server.py
@@ -9,8 +9,7 @@ from rest_framework.mixins import RetrieveModelMixin
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
-from bublik.core.config.services import ConfigServices
-from bublik.data.models import GlobalConfigs
+from bublik.core.project import get_current_project
 
 
 __all__ = [
@@ -23,13 +22,9 @@ class ServerViewSet(RetrieveModelMixin, GenericViewSet):
 
     @action(detail=False, methods=['get'])
     def project(self, request):
-        # TODO: The existence of PROJECT in per_conf should be checked when the server starts
         return Response(
             {
-                'project': ConfigServices.getattr_from_global(
-                    GlobalConfigs.PER_CONF.name,
-                    'PROJECT',
-                ),
+                'project': get_current_project(),
             },
         )
 
