6f325a2a807d159a07ecec523e62de0dddddf7b0
Natalia Rybchenko <natalia.rybchenko@oktetlabs.ru>
2025-05-21 16:32:23 +0300

    history: extend trend charts view data with measurement series chart data
    Extend the history trend charts view data with measurement series chart
    data to enable stacked chart rendering in the UI.
    
    Related to #113
    Signed-off-by: Natalia Rybchenko <natalia.rybchenko@oktetlabs.ru>

--- bublik/interfaces/api_v2/measurements.py
+++ bublik/interfaces/api_v2/measurements.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright (C) 2016-2023 OKTET Labs Ltd. All rights reserved.
 
+from collections import OrderedDict
 import typing
 
 from rest_framework import status
@@ -9,9 +10,9 @@ from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from bublik.core.measurement.representation import ChartViewBuilder
-from bublik.core.measurement.services import get_measurement_results
-from bublik.core.utils import unordered_group_by
-from bublik.data.models import Measurement
+from bublik.core.measurement.services import get_measurement_charts, get_measurement_results
+from bublik.core.utils import key_value_dict_transforming, unordered_group_by
+from bublik.data.models import Measurement, TestIterationResult
 from bublik.data.serializers import MeasurementSerializer
 
 
@@ -41,16 +42,45 @@ class MeasurementViewSet(GenericViewSet):
         mmrs = get_measurement_results(result_ids)
         mmrs_groups = unordered_group_by(mmrs, 'measurement_group_key')
 
-        return Response(
-            [
-                (
-                    ChartViewBuilder(next(iter(mmr_group)).measurement).by_measurement_results(
-                        mmr_group,
-                    )
-                ).representation()
-                for _mm_key, mmr_group in mmrs_groups.items()
-            ],
-        )
+        trend_charts = [
+            (
+                ChartViewBuilder(next(iter(mmr_group)).measurement).by_measurement_results(
+                    mmr_group,
+                )
+            ).representation()
+            for _mm_key, mmr_group in mmrs_groups.items()
+        ]
+
+        measurement_series_charts_by_result = []
+        for result_id in result_ids:
+            test_result = TestIterationResult.objects.get(id=result_id)
+            parameters = {}
+            for test_argument in test_result.iteration.test_arguments.all():
+                parameters[test_argument.name] = test_argument.value
+            parameters = OrderedDict(sorted(parameters.items()))
+            parameters_list = key_value_dict_transforming(parameters)
+
+            measurement_series_charts = []
+            for chart in get_measurement_charts(result_id):
+                chart['id'] = f'{test_result.id}_{chart['id']}'
+                measurement_series_charts.append(chart)
+
+            measurement_series_charts_by_result.append(
+                {
+                    'id': test_result.id,
+                    'start': test_result.start,
+                    'test_name': test_result.iteration.test.name,
+                    'parameters_list': parameters_list,
+                    'measurement_series_charts': measurement_series_charts,
+                },
+            )
+
+        data = {
+            'trend_charts': trend_charts,
+            'measurement_series_charts_by_result': measurement_series_charts_by_result,
+        }
+
+        return Response(data)
 
     def retrieve(self, request, pk=None):
         measurement = self.get_object()
