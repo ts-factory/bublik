91d66becb36b8e444e4572c60d2800405e3dd074
Natalia Rybchenko <natalia.rybchenko@oktetlabs.ru>
2025-05-21 16:32:23 +0300

    measurements: add API for per-result series measurement charts
    Add API for per-result measurement series to enable stacked chart
    rendering in the UI.
    
    Related to #113
    Signed-off-by: Natalia Rybchenko <natalia.rybchenko@oktetlabs.ru>

--- bublik/interfaces/api_v2/measurements.py
+++ bublik/interfaces/api_v2/measurements.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright (C) 2016-2023 OKTET Labs Ltd. All rights reserved.
 
+from collections import OrderedDict
 import typing
 
 from rest_framework import status
@@ -9,9 +10,9 @@ from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from bublik.core.measurement.representation import ChartViewBuilder
-from bublik.core.measurement.services import get_measurement_results
-from bublik.core.utils import unordered_group_by
-from bublik.data.models import Measurement
+from bublik.core.measurement.services import get_measurement_charts, get_measurement_results
+from bublik.core.utils import key_value_dict_transforming, unordered_group_by
+from bublik.data.models import Measurement, TestIterationResult
 from bublik.data.serializers import MeasurementSerializer
 
 
@@ -51,6 +52,45 @@ class MeasurementViewSet(GenericViewSet):
                 for _mm_key, mmr_group in mmrs_groups.items()
             ],
         )
+
+    @action(detail=False, methods=['post'])
+    def by_result_ids(self, request):
+        result_ids = request.data.get('result_ids', None)
+        if not result_ids:
+            return Response(
+                status=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                data={'type': 'ValueError', 'message': 'No result IDs specified'},
+            )
+
+        measurement_series_charts_by_result = []
+        for result_id in result_ids:
+            test_result = TestIterationResult.objects.get(id=result_id)
+
+            # get parameters
+            parameters = {}
+            for test_argument in test_result.iteration.test_arguments.all():
+                parameters[test_argument.name] = test_argument.value
+            parameters = OrderedDict(sorted(parameters.items()))
+            parameters_list = key_value_dict_transforming(parameters)
+
+            # get chart data
+            measurement_series_charts = []
+            for chart in get_measurement_charts(result_id):
+                chart['id'] = f'{test_result.id}_{chart["id"]}'
+                measurement_series_charts.append(chart)
+
+            measurement_series_charts_by_result.append(
+                {
+                    'run_id': test_result.test_run_id,
+                    'result_id': test_result.id,
+                    'start': test_result.start,
+                    'test_name': test_result.iteration.test.name,
+                    'parameters_list': parameters_list,
+                    'measurement_series_charts': measurement_series_charts,
+                },
+            )
+
+        return Response(measurement_series_charts_by_result)
 
     def retrieve(self, request, pk=None):
         measurement = self.get_object()
