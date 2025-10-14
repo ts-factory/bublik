e74cb1d0d4b0ce31906cafc0c0ef7f1a9e7020b8
Natalia Rybchenko <natalia.rybchenko@oktetlabs.ru>
2025-10-14 17:54:45 +0300

    importruns: ensure project query param consistency across APIs
    Use 'project' query parameter instead of 'project_name' in importruns
    API for consistency.
    
    Signed-off-by: Natalia Rybchenko <natalia.rybchenko@oktetlabs.ru>

--- bublik/interfaces/api_v2/importruns.py
+++ bublik/interfaces/api_v2/importruns.py
@@ -18,6 +18,7 @@ from bublik.core.importruns.live.context import LiveLogContext, LiveLogError
 from bublik.core.importruns.utils import indicate_collision
 from bublik.core.shortcuts import build_absolute_uri, get_current_scheme_host_prefix
 from bublik.core.utils import get_local_log
+from bublik.data.models import Project
 from bublik.interfaces.celery import tasks
 
 
@@ -40,7 +41,9 @@ class ImportrunsViewSet(ViewSet):
         param_from = request.query_params.get('from', '').replace('-', '.')
         param_to = request.query_params.get('to', '').replace('-', '.')
         param_force = request.query_params.get('force', 'false')
-        param_project_name = request.query_params.get('project_name')
+
+        project_id = request.query_params.get('project')
+        param_project_name = Project.objects.get(id=project_id).name if project_id else None
 
         try:
             requesting_host = get_current_scheme_host_prefix(request)
