95f85f52fd4028b32c59cb0773dc2b00785d8729
Mikhail Zayats <mikhail.zayats@oktetlabs.ru>
2025-11-17 09:35:00 +0000

    config: add serializers for config docs
    Add base for serializers for config documentation
    
    Signed-off-by: Mikhail Zayats <mikhail.zayats@oktetlabs.ru>

---
+++ bublik/data/serializers/docs/config.py
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2025 OKTET Labs Ltd. All rights reserved.
+
+from rest_framework import serializers
+
+
+class AllVersionsResponseSerializer(serializers.Serializer):
+    pass
+
+
+class AvailableTypesNamesResponseSerializer(serializers.Serializer):
+    pass
