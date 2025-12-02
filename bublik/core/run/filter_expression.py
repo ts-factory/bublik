e8aa04cc625207c1fc4755473a73dcf66056b514
Natalia Rybchenko <natalia.rybchenko@oktetlabs.ru>
2025-12-02 08:54:51 +0300

    run: fix compatibility issues by replacing outdated PyEDA with SymPy
    PyEDA is outdated and causes compatibility issues. Replace it with SymPy
    and update filtering by expression accordingly.
    
    Signed-off-by: Natalia Rybchenko <natalia.rybchenko@oktetlabs.ru>

--- bublik/core/run/filter_expression.py
+++ bublik/core/run/filter_expression.py
@@ -1,12 +1,13 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright (C) 2016-2023 OKTET Labs Ltd. All rights reserved.
 
-import copy
+from copy import deepcopy
 import os
 
 from django.db.models import Q
-import pyeda.inter
 import pyparsing as pp
+from sympy import Symbol
+from sympy.logic.boolalg import And, Not, Or, to_dnf
 
 from bublik.data.models import TestIterationResult
 
@@ -183,11 +184,11 @@ class TestRunMetasGroup:
 
             for smeta in self.metas:
                 if str(smeta) == str(meta):
-                    return smeta.alias
+                    return Symbol(smeta.alias)
 
             meta.alias = 'talias' + str(len(self.metas))
             self.metas_append(meta)
-            return meta.alias
+            return Symbol(meta.alias)
 
         if not expr_str:
             return None
@@ -226,14 +227,14 @@ class TestRunMetasGroup:
         prec = pp.infixNotation(
             condition,
             [
-                ('!', 1, pp.opAssoc.RIGHT),
-                ('&', 2, pp.opAssoc.LEFT),
-                ('|', 2, pp.opAssoc.LEFT),
+                ('!', 1, pp.opAssoc.RIGHT, lambda t: Not(t[0][1])),
+                ('&', 2, pp.opAssoc.LEFT, lambda t: And(t[0][0], t[0][2])),
+                ('|', 2, pp.opAssoc.LEFT, lambda t: Or(t[0][0], t[0][2])),
             ],
         )
 
         try:
-            res = prec.parseString(expr_str, parseAll=True)
+            sympy_expr = prec.parseString(expr_str, parseAll=True)[0]
         except pp.ParseException as pe:
             if expr_type == 'verdict':
                 expected = 'None | "Verdict"'
@@ -247,61 +248,35 @@ class TestRunMetasGroup:
             )
             raise pp.ParseException(msg) from pe
 
-        line_esc = ''
-
-        def parse_item(item):
-            line_local = ''
-            if isinstance(item, pp.ParseResults):
-                line_local += '('
-                for val in item:
-                    line_local += parse_item(val)
-                line_local += ')'
-            else:
-                if item == '!':
-                    item = '~'
-                line_local += item
-            return line_local
-
-        line_esc = parse_item(res)
-        return pyeda.inter.expr(line_esc).to_dnf()
+        return to_dnf(sympy_expr, simplify=True)
 
     def apply_filters(self, qs, expr_dnf, expr_type):
         def filter_meta(qs, var):
-            meta = None
-            if isinstance(var, pyeda.boolalg.expr.Variable):
+
+            if isinstance(var, Symbol):
                 meta = self.get_meta_by_alias(str(var))
-                qs = qs.filter(meta.filter_q(expr_type=expr_type))
-            elif isinstance(var, pyeda.boolalg.expr.Complement):
-                meta = self.get_meta_by_alias(str(var)[1::])
-                qs = qs.filter(meta.filter_q(negation=True, expr_type=expr_type))
-            elif isinstance(var, pyeda.boolalg.expr.AndOp):
-                for item in var.xs:
+                return qs.filter(meta.filter_q(expr_type=expr_type))
+
+            if isinstance(var, Not):
+                meta = self.get_meta_by_alias(str(var.args[0]))
+                return qs.filter(meta.filter_q(negation=True, expr_type=expr_type))
+
+            if isinstance(var, And):
+                for item in var.args:
                     qs = filter_meta(qs, item)
-            else:
-                msg = f'Unknown variable type {type(var)}: \'{var!s}\''
-                raise TypeError(msg)
+                return qs
 
-            return qs
+            if isinstance(var, Or):
+                qs_list = [filter_meta(deepcopy(qs), item) for item in var.args]
+                all_ids = set()
+                for qsi in qs_list:
+                    all_ids.update(qsi.values_list('id', flat=True))
+                return TestIterationResult.objects.filter(id__in=list(all_ids))
 
-        base_qs = qs
-        qs = None
+            msg = f'Unknown variable type {type(var)}: {var!r}'
+            raise TypeError(msg)
 
-        if isinstance(expr_dnf, pyeda.boolalg.expr.OrOp):
-            for item in expr_dnf.xs:
-                qs2 = copy.deepcopy(base_qs)
-                qs2 = filter_meta(qs2, item)
-                if qs:
-                    # The resulting QS should not be a union, because this will make further
-                    # filtering impossible (calling filter() for union is not supported)
-                    qs = TestIterationResult.objects.filter(
-                        id__in=list(qs.union(qs2).values_list('id', flat=True)),
-                    )
-                else:
-                    qs = qs2
-        else:
-            qs = filter_meta(base_qs, expr_dnf)
-
-        return qs
+        return filter_meta(qs, expr_dnf)
 
 
 def filter_by_expression(filtered_qs, expr_str, expr_type=None):
