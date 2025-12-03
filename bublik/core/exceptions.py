d2e8368fb81cb0126327f148f75ab9c5417b896a
Natalia Rybchenko <natalia.rybchenko@oktetlabs.ru>
2025-12-03 07:56:32 +0300

    exceptions: introduce ImportrunsError to improve importruns error reporting
    Introduce ImportrunsError to make it possible to provide safe and
    informative error messages on importruns failures.
    
    Signed-off-by: Natalia Rybchenko <natalia.rybchenko@oktetlabs.ru>

--- bublik/core/exceptions.py
+++ bublik/core/exceptions.py
@@ -60,6 +60,10 @@ class BublikServerError(BublikError):
     pass
 
 
+class ImportrunsError(BublikServerError):
+    pass
+
+
 class BublikAPIError(BublikError, APIException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_detail = 'Something went wrong, please try again'
