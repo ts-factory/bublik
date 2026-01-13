f555a3f6990be861d9bd7f26e0181d09c5a58a10
Danil Kostromin <danil.kostromin@icloud.com>
2026-01-13 12:55:14 +0300

    refactor: add `ServerService` for version information
    Introduce dedicated service for retrieving version information.
    Encapsulates system metadata queries to provide consistent version
    info across different interfaces.
    
    Signed-off-by: Danil Kostromin <danil.kostromin@icloud.com>

---
+++ bublik/core/server/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2025-2026 OKTET Labs Ltd. All rights reserved.
+
+from bublik.core.server.services import ServerService
+
+
+__all__ = ['ServerService']
