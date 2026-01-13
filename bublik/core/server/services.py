a818ec4282b6eceaa82e52f2bae5b9c5f372189a
Danil Kostromin <danil.kostromin@icloud.com>
2026-01-13 12:55:14 +0300

    server: extract server related operations for separation of concern
    Introduce dedicated service for retrieving version information.
    Encapsulates system metadata queries to provide consistent version
    info across different interfaces.
    
    Signed-off-by: Danil Kostromin <danil.kostromin@icloud.com>

---
+++ bublik/core/server/services.py
@@ -0,0 +1,19 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2025-2026 OKTET Labs Ltd. All rights reserved.
+
+from __future__ import annotations
+
+from django.conf import settings
+
+
+class ServerService:
+
+    @staticmethod
+    def get_version() -> dict:
+        '''
+        Get server version information.
+
+        Returns:
+            Dictionary with repository revision information
+        '''
+        return settings.REPO_REVISIONS
