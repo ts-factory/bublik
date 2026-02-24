1daab336a62c9f06eb9b06706d78612aee8ce7f0
Danil Kostromin <danil.kostromin@icloud.com>
2026-02-25 02:53:18 +0300

    project: adjust project API routes to use service layer
    Signed-off-by: Danil Kostromin <danil.kostromin@icloud.com>

--- bublik/interfaces/api_v2/project.py
+++ bublik/interfaces/api_v2/project.py
@@ -1,34 +1,60 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright (C) 2025 OKTET Labs Ltd. All rights reserved.
 
-from rest_framework.exceptions import ValidationError
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from bublik.core.auth import auth_required
-from bublik.data.models import Project
+from bublik.core.project import ProjectService
 from bublik.data.serializers import ProjectSerializer
 
 
 class ProjectViewSet(ModelViewSet):
-    queryset = Project.objects.all().order_by('id')
     serializer_class = ProjectSerializer
+
+    def get_queryset(self):
+        return ProjectService.list_projects_queryset()
 
     @auth_required(as_admin=True)
     def create(self, request, *args, **kwargs):
-        return super().create(request, *args, **kwargs)
+        project = ProjectService.create_project(request.data)
+        serializer = self.get_serializer(project)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def retrieve(self, request, *args, **kwargs):
+        project = self.get_object()
+        project = ProjectService.get_project_instance(project.id)
+        serializer = self.get_serializer(project)
+        return Response(serializer.data)
 
     @auth_required(as_admin=True)
     def update(self, request, *args, **kwargs):
-        return super().update(request, *args, **kwargs)
+        project = self.get_object()
+        project = ProjectService.update_project(
+            project_id=project.id,
+            data=request.data,
+            partial=False,
+        )
+        serializer = self.get_serializer(project)
+        return Response(serializer.data)
+
+    @auth_required(as_admin=True)
+    def partial_update(self, request, *args, **kwargs):
+        project = self.get_object()
+        project = ProjectService.update_project(
+            project_id=project.id,
+            data=request.data,
+            partial=True,
+        )
+        serializer = self.get_serializer(project)
+        return Response(serializer.data)
 
     @auth_required(as_admin=True)
     def destroy(self, request, *args, **kwargs):
         project = self.get_object()
-        if project.testiterationresult_set.exists():
-            msg = 'Unable to delete: there are runs linked to this project'
-            raise ValidationError(msg)
-        return super().destroy(request, *args, **kwargs)
+        ProjectService.delete_project(project.id)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
