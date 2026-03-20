c93a8320b113add6f01d7035c33ce4d7b1b7be2a
Natalia Rybchenko <natalia.rybchenko@oktetlabs.ru>
2026-03-20 09:55:18 +0300

    importruns: extract run path traversal logic to separate concerns
    Move the run path traversal logic out of the importruns management
    command into a separate module to reduce complexity and separate
    concerns.
    
    Signed-off-by: Natalia Rybchenko <natalia.rybchenko@oktetlabs.ru>

--- bublik/interfaces/management/commands/importruns.py
+++ bublik/interfaces/management/commands/importruns.py
@@ -4,12 +4,9 @@
 from datetime import datetime
 from functools import wraps
 import os
-import re
 import shutil
 import tempfile
-from urllib.parse import urljoin
 
-from bs4 import BeautifulSoup
 from django.core.management.base import BaseCommand
 import pendulum
 
@@ -24,91 +21,26 @@ from bublik.core.config.services import ConfigServices
 from bublik.core.exceptions import (
     ImportrunsError,
     RunAlreadyExistsError,
-    RunCompromisedError,
     RunOutsidePeriodError,
-    URLFetchError,
 )
 from bublik.core.importruns import categorization, extract_logs_base
 from bublik.core.importruns.source import incremental_import
+from bublik.core.importruns.source.run_traversal import (
+    HTTPDirectoryTraverser,
+    with_path_processing_events,
+)
 from bublik.core.importruns.telog import JSONLog
 from bublik.core.importruns.utils import runtime
 from bublik.core.logging import get_task_or_server_logger
 from bublik.core.run.actions import prepare_cache_for_completed_run
 from bublik.core.run.metadata import MetaData
 from bublik.core.run.objects import add_import_id, add_run_log
-from bublik.core.url import fetch_url, save_url_to_dir
-from bublik.core.utils import Counter, create_event
+from bublik.core.url import save_url_to_dir
+from bublik.core.utils import create_event
 from bublik.data.models import EventLog, GlobalConfigs, Project
 
 
 logger = get_task_or_server_logger()
-
-
-def with_path_processing_events(func):
-    @wraps(func)
-    def wrapper(self, *args, **options):
-        start_time = datetime.now()
-
-        # Log URLs without a training slash are valid, but they don't pass
-        # Kerberos authorization check (bug 11109, bug 11168).
-        init_url = options['url']
-        if not init_url.endswith('/'):
-            init_url += '/'
-            options['url'] = init_url
-
-        task_id = options['task_id']
-        task_msg = f'Celery task ID {task_id}' if task_id else 'No Celery task ID'
-
-        create_event(
-            facility=EventLog.FacilityChoices.IMPORTRUNS,
-            severity=EventLog.SeverityChoices.INFO,
-            msg=f'started processing the path {init_url} -- {task_msg}',
-        )
-
-        counter = Counter()
-        try:
-            for _run_url in func(self, *args, **options):
-                counter.increment()
-        except (URLFetchError, RunCompromisedError) as e:
-            # Update exception debug details with init url
-            debug_details = getattr(e, 'debug_details', [])
-            debug_details.append(f'Init URL: {init_url}')
-            e.debug_details = debug_details
-
-            event_msg = (
-                f'failed import {init_url} '
-                f'-- {task_msg} '
-                f'-- Error: {e.message} '
-                f'-- runtime: {runtime(start_time)} sec'
-            )
-
-            severity = (
-                EventLog.SeverityChoices.ERR
-                if isinstance(e, URLFetchError)
-                else EventLog.SeverityChoices.WARNING
-            )
-            logger_func = logger.error if isinstance(e, URLFetchError) else logger.warning
-            logger_func(f'Importruns failed: {e.message}. Ignoring: {init_url}', exc_info=e)
-
-            create_event(
-                facility=EventLog.FacilityChoices.IMPORTRUNS,
-                severity=severity,
-                msg=event_msg,
-            )
-        finally:
-            create_event(
-                facility=EventLog.FacilityChoices.IMPORTRUNS,
-                severity=EventLog.SeverityChoices.INFO,
-                msg=(
-                    f'finished processing the path {init_url} '
-                    f'-- {task_msg} '
-                    f'-- {counter.counter} sessions were processed '
-                    f'-- runtime: {runtime(start_time)} sec'
-                ),
-            )
-            logger.info(f'completed in [{datetime.now() - start_time}]')
-
-    return wrapper
 
 
 def with_import_events(func):
@@ -179,43 +111,6 @@ def with_import_events(func):
             return None
 
     return wrapper
-
-
-class HTTPDirectoryTraverser:
-    def __init__(self, url):
-        super().__init__()
-        self.url = url
-
-    def __find_runs(
-        self,
-        url,
-    ):
-        html = fetch_url(url, quiet_404=True)
-
-        ast = BeautifulSoup(markup=html, features='html.parser')
-        if ast.find(
-            lambda t: t.name == 'a' and t.string.strip().lower() == 'trc_compromised.js',
-        ):
-            raise RunCompromisedError
-
-        if check_run_file('meta_data.json', url):
-            yield url
-            return
-
-        # NOTE: the parser relies on links to directories ending with a slash "/"
-        for node in ast.find_all(
-            lambda t: t.name == 'a'
-            and hasattr(t, 'href')
-            and not re.match(r'(\./)?\.\./?', t['href'])
-            and t['href'].endswith('/'),
-        ):
-            a_href = node['href'].strip()
-            url_next = urljoin(url + '/', a_href)
-
-            yield from self.__find_runs(url_next)
-
-    def find_runs(self):
-        yield from self.__find_runs(self.url)
 
 
 class Command(BaseCommand):
