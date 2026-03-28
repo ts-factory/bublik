dbe5f0c43daf3f89e6989119abfb161637add753
Natalia Rybchenko <natalia.rybchenko@oktetlabs.ru>
2026-03-28 18:53:53 +0300

    jobtasks: add task execution service for separation of concerns
    Introduce dedicated service for retrieving job task execution data.
    Encapsulates task status, runtime, event logs and error extraction
    to provide consistent session import tracking across interfaces.
    
    Signed-off-by: Natalia Rybchenko <natalia.rybchenko@oktetlabs.ru>

---
+++ bublik/core/job_task/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2026 OKTET Labs Ltd. All rights reserved.
+
+from bublik.core.job_task.services import JobTaskExecutionService
+
+
+__all__ = ['JobTaskExecutionService']
